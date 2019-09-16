17b9daf66f6fa03d43299f19ccf381118ba5fd11
Adam Jones <adam@linuxtampa.com>
2019-09-16 19:17:54 -0400

    Setup is done and wrote first query

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace SafariVacation
+{
+  class Program
+  {
+    public static void ReadData()
+    {
+      var db = new SafariContext();
+      var allAnimalsSeen = db.Safaris.Where(s => s.CountOfTimesSeen <= 5);
+    }
+    static void Main(string[] args)
+    {
+
+    }
+  }
+}
---
+++ SafariContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using SafariVacation;
+
+namespace SafariVacation
+{
+  public partial class SafariContext : DbContext
+  {
+    public DbSet<safari> Animals { get; set; }
+    public SafariContext()
+    {
+    }
+
+    public SafariContext(DbContextOptions<SafariContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      if (!optionsBuilder.IsConfigured)
+      {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+        optionsBuilder.UseNpgsql("server=localhost;database=safaridb");
+      }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+    }
+    public DbSet<safari> Safaris { get; set; }
+  }
+}
